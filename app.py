1f023f3c4e917f5e93a985f3347fe83c98c7ff99
Anand <asb761@nyu.edu>
2018-05-06 12:01:13 -0400

    more accurate results & display

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ import dill as pickle
 from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics.pairwise import cosine_similarity
+import random
 
 app = Flask(__name__)
 
 numerical = ['GPA_x', 'SAT_MATH', 'SAT_CRITICAL', 'SAT_WRITING','SAT_TOTAL',
-             'ACT_MAIN','Class_Rank','Recommendations','Essay','Extracurricular','Work_Exp']
+             'ACT_MAIN','Class_Rank','Recommendations','Essay','Extracurricular','Work_Exp','Acceptance_Rates']
 
 @app.route('/')
 def home():
@@ -21,34 +22,52 @@ def suggest_colleges():
     if request.method=='POST':
         result=request.form
         #print(result['math'])
-        total = result['math']+result['reading']+result['writing']
+        total = float(result['math'])+float(result['reading'])+float(result['writing'])
+        print(total)
         #print('Upto here')
-	test_sample = np.array([result['gpa'],result['math'],result['reading'],result['writing'],
-                                total,result['act'],result['rank'],result['reco'],
-                                result['essay'],result['extra'],result['workex']])
+	test_sample_pred = np.array([float(result['gpa']),float(result['math']),float(result['reading']),float(result['writing']),
+                                total,float(result['act']),float(result['rank']),float(result['reco']),
+                                float(result['essay']),float(result['extra']),float(result['workex'])])
 
-	test_sample = np.reshape(test_sample,(1,-1))
+	test_sample_pred = np.reshape(test_sample_pred,(1,-1))
         
         pkl_file = open('./models/model_rf_v1.pk', 'rb')
         rfmodel = pickle.load(pkl_file)
-        prediction = rfmodel.predict(test_sample)
+        prediction = rfmodel.predict(test_sample_pred)
 
         print(prediction)
-        
+
+        if prediction==5:
+            ar= random.uniform(0.07,0.2)
+        elif prediction==4:
+            ar= random.uniform(0.2,0.4)
+        elif prediction==3:
+            ar= random.uniform(0.4,0.6)
+        elif prediction==2:
+            ar= random.uniform(0.6,0.8)
+        else:
+            ar= random.uniform(0.8,1)
+
+        test_sample = np.array([float(result['gpa']),float(result['math']),float(result['reading']),float(result['writing']),
+                                total,float(result['act']),float(result['rank']),float(result['reco']),
+                                float(result['essay']),float(result['extra']),float(result['workex']),ar])
+        test_sample = np.reshape(test_sample,(1,-1))
         class1=[]
         class2=[]
         class3=[]
 
         college = pd.read_pickle('./models/college_data.pkl')
         sc = MinMaxScaler(feature_range=(0,1))
-        sc.fit(college[numerical])
+        #sc.fit(college[numerical])
         
-        college[numerical] = sc.transform(college[numerical])
+        #college[numerical] = sc.transform(college[numerical])
         print(college.head())
-        test_sample = sc.transform(test_sample)
-        test_sample = np.clip(test_sample,0,1)
-        
         print(test_sample)
+        #test_sample = sc.transform(test_sample)
+        print(test_sample)
+        #test_sample = np.clip(test_sample,0,1)
+        
+        #print(test_sample)
         
         if prediction == 5:
             college_ambi = college[college['Label']==5]
@@ -117,7 +136,6 @@ def suggest_colleges():
             for i in class1:
                 templ.append(i.split(",")[0])
 
-            
             college_mod = college[(college['Label']==2) & (~college['University Name'].isin(templ))]
             class2 = helper(college_mod,test_sample)
             
@@ -125,9 +143,9 @@ def suggest_colleges():
             class3 = helper(college_safe,test_sample)
             
 
-        final_dict = {'Ambitious: ':class1,'Moderate: ':class2,'Safe: ':class3}
+        #final_dict = {'Moderate: ':class2,'Ambitious: ':class1,'Safe: ':class3}
         
-        return render_template('result.html',prediction=final_dict)
+        return render_template('result.html',ambi=class1,mod=class2,safe=class3)
 
 def helper(df,test):
     l=[]
@@ -140,6 +158,10 @@ def helper(df,test):
     l = np.array(l)
     a = l.argsort()[:5][::1]
     cl=[]
+    #print("======================")
+    #for i in a:
+    #    print i,l[i]
+    
     for i in a:
         cl.append(str(df.iloc[i,0])+", "+str(df.iloc[i,3]))
 
